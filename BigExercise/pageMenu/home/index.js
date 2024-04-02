a14ddc13ee552e38a510ddecc9971cd2cab5b60b
Mr.Qui <levanquy1923@gmail.com>
2024-04-02 13:22:34 +0700

    chuyen header, footer ra file rieng

--- BigExercise/pageMenu/home/index.js
+++ BigExercise/pageMenu/home/index.js
@@ -1,31 +1,61 @@
-$(document).ready(function () {
-    $(".btnDeleteSp").click(function () {
-        $(this).parent().remove()
+// số đếm
+var hoursDisplay = document.getElementById("hours");
+var minutesDisplay = document.getElementById("minutes");
+var secondsDisplay = document.getElementById("seconds");
 
-    });
+function SetCountdown(hours, minutes, seconds) {
+    var totalSeconds = hours * 3600 + minutes * 60 + seconds;
 
-    // +-sp
-    $(".add").click(function () {
-        var number =$(this).parent().find(".number")
-        var currentValue =parseInt(number.text())
-        currentValue= currentValue+1;
-        ($(this).parent().find(".number").text(currentValue));
+    var Countdown = setInterval(function() {
+        let hour = Math.floor(totalSeconds / 3600);
+        let minute = Math.floor((totalSeconds % 3600) / 60);
+        let second = totalSeconds % 60;
 
-    });
+        hoursDisplay.innerHTML = pad(hour);
+        minutesDisplay.innerHTML = pad(minute);
+        secondsDisplay.innerHTML = pad(second);
 
-    $(".sub").click(function () {
-        var number =$(this).parent().find(".number")
-        var currentValue =parseInt(number.text())
-        if (currentValue > 1) {
-            currentValue= currentValue-1;
-            ($(this).parent().find(".number").text(currentValue));
+        totalSeconds--;
 
+        if (totalSeconds < 0) {
+            clearInterval(Countdown);
         }
-    });
+    }, 1000);
+}
 
-    document.getElementById("paymentButton").addEventListener("click", function() {
-        window.location.href = "../../permenent/cart/index.html";
-    });
-   
+function pad(para) {
+    return (para < 10 ? "0" : "") + para;
+}
+
+SetCountdown(1, 20, 10);
+
+// copy Vorcher
+
+
+var copyVorcher = document.querySelectorAll(".copyVorcher")
+copyVorcher.forEach(function (button) {
+    button.addEventListener("click",function(){
+    var copied =button.parentNode.querySelector('#nameVorcher');
+    if(copied){
+        copyToClipboard(copied.textContent);
+        alert("mã " +copied.textContent +" đã được copy")
+    }else{
+        alert("mã" +copied.textContent + "đã hết")
+    }
+    })    
 });
+
+function copyToClipboard(copyText) {
+    var textArea = document.createElement("textarea");
+    textArea.value = copyText;
+    document.body.appendChild(textArea);
+    textArea.select();
+    document.execCommand("copy");
+    document.body.removeChild(textArea)
+
+}
+
+
+
+
 
